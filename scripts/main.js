041d078165c8f65a88459e2b212ddeeb12f5b7a2
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-09-02 16:57:23 -0600

    add routes

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+var helpers = require('./helpers');
 
 var App = React.createClass({
   render: function() {
@@ -58,11 +63,28 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" required/>
+        <input type="text" ref="storeId" defaultValue={helpers.getFunName()}/>
         <input type="submit"/>
       </form>
     )
   }
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+var NotFound = React.createClass({
+  render: function() {
+    return (
+      <h1>404 Not Found!</h1>
+    )
+  }
+});
+
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker}/>
+    <Route path="/store/:storeId" component={App}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
